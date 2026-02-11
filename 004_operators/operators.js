0e67d603f175bd20466b10e878155ee01768117a
Naveen Sahu <145324584+Naveen02-s@users.noreply.github.com>
2026-02-11 08:06:22 +0000

    Operators

---
+++ 004_operators/operators.js
@@ -0,0 +1,11 @@
+// console.log(5+3);
+// console.log(5-3);
+// console.log(5*3);
+// console.log(5**3);
+// console.log(5/3);
+// console.log(5%3);
+
+console.log(1 + "2");
+console.log("1" + 2);
+console.log("1" + 2 + 3);
+console.log(1 + 2 + "3");
