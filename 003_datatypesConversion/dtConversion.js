56fd4fb1a55d2447bd6557d0a39c241ad12f9be5
Naveen Sahu <145324584+Naveen02-s@users.noreply.github.com>
2026-02-11 04:32:24 +0000

    Conversion of datatypes

---
+++ 003_datatypesConversion/dtConversion.js
@@ -0,0 +1,10 @@
+let var1 = " "
+
+// let var1InNumber = Number(var1)
+// let var1InString = String(var1)
+let var1InBoolean = Boolean(var1)
+
+console.log(typeof var1InBoolean);
+console.log(var1InBoolean);
+
+
