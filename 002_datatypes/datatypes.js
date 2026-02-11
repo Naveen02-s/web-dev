0f66d943a12ba5f1946fcd4657c00c31a690c700
Naveen Sahu <145324584+Naveen02-s@users.noreply.github.com>
2026-02-11 03:47:44 +0000

    datatypes

---
+++ 002_datatypes/datatypes.js
@@ -0,0 +1,9 @@
+let userName = "Something"
+let age = 18
+let isLoggedIn = true
+let var1;
+let var2 = null
+
+console.table([typeof(userName), typeof(age), typeof(isLoggedIn), typeof(var1), typeof(var2)])
+
+console.log(var2)
