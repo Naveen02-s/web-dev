6a9064a848e800e59cc9a9a3f8982c798935651b
Naveen <naveensahu1402@gmail.com>
2026-02-12 14:04:01 +0530

    type of datatypes

--- 002_datatypes/datatypes.js
+++ 002_datatypes/datatypes.js
@@ -3,7 +3,26 @@ let age = 18
 let isLoggedIn = true
 let var1;
 let var2 = null
+let bigNumber = 1234567890123456789012345678901234567890123456789012345678901234567890n
 
-console.table([typeof(userName), typeof(age), typeof(isLoggedIn), typeof(var1), typeof(var2)])
 
-console.log(var2)
+// console.table([typeof(userName), typeof(age), typeof(isLoggedIn), typeof(var1), typeof(var2)])
+
+// console.log(var2)
+
+let obj = {
+    userName : "Something",
+    age : 18,
+    isLoggedIn : true
+}
+
+let func = function() {
+    console.log("Hello!")
+}
+
+let arr = ["1", "2", "3"]
+
+console.log(typeof obj)
+console.log(typeof func)
+console.log(typeof arr)
+console.log(typeof bigNumber)
