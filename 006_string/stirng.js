082bfbaf05a80be5ee2a900f4baa7acc805c8f68
Naveen Sahu <145324584+Naveen02-s@users.noreply.github.com>
2026-02-18 08:31:49 +0000

    String

---
+++ 006_string/stirng.js
@@ -0,0 +1,18 @@
+const gameName = new String("hitesh  hh")
+const userName = "as sd df fg gh"
+// console.log(typeof gameName);
+// console.log(typeof userName);
+
+// console.log(gameName.length);
+
+// console.log(gameName.__proto__);
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('a'));
+// console.log(gameName.italics());
+// console.log(gameName.sub());
+// console.log(gameName.sub());
+console.log(gameName);
+// console.log(userName.trim());
+// console.log(gameName.slice(0, 4));
+// console.log(gameName.replace('h', 'H'));
+console.log(userName.split(' '))
