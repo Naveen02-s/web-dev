4c4fcdeac27333ef6e1d8de85b9e3e78aec84f6f
Naveen Sahu <145324584+Naveen02-s@users.noreply.github.com>
2026-02-19 04:50:02 +0000

    Numbers

---
+++ Javascript/007_numbers_&_maths/nums_maths.js
@@ -0,0 +1,17 @@
+const score = 400
+const score2 = new Number(400)
+
+// console.log(score);
+// console.log(typeof score);
+
+// console.log(score2);
+// console.log(typeof score2);
+
+// console.log(score2.toString());
+// console.log(typeof score2.toString());
+
+// console.log(score.toFixed(3));
+
+// const number = 100000
+// console.log(number.toLocaleString('en-IN'));
+
