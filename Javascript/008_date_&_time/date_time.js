0215ab5566704c87a393fd8a50bd5440158982ef
Naveen Sahu <145324584+Naveen02-s@users.noreply.github.com>
2026-02-19 09:28:57 +0000

    Date

---
+++ Javascript/008_date_&_time/date_time.js
@@ -0,0 +1,8 @@
+const myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+
+console.log(typeof myDate);
