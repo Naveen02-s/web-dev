8e2cc74bd0da13c6cde130a6039d85a9f73a1859
Naveen Sahu <145324584+Naveen02-s@users.noreply.github.com>
2026-02-19 08:21:47 +0000

    Math and numbers

--- Javascript/007_numbers_&_maths/nums_maths.js
+++ Javascript/007_numbers_&_maths/nums_maths.js
@@ -15,3 +15,23 @@ const score2 = new Number(400)
 // const number = 100000
 // console.log(number.toLocaleString('en-IN'));
 
+
+/*--------------------MATHS--------------------*/
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(43.5));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(8, 5, 9, 3, 4));
+// console.log(Math.max(8, 5, 9, 3, 4));
+
+// const randomVal = Math.random()
+// console.log(randomVal);
+// console.log(randomVal * 10);
+// console.log((randomVal * 10) + 1);
+// console.log(Math.floor((randomVal * 10) + 1));
+
+const min = 10
+const max = 20
+
+console.log(Math.random() * (max - min + 1) + min);
