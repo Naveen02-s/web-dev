fe4c99227f613ea27f383487f0cbf94267d8bda9
Naveen Sahu <145324584+Naveen02-s@users.noreply.github.com>
2026-02-23 07:05:13 +0000

    Array

---
+++ Javascript/009_array/array.js
@@ -0,0 +1,40 @@
+const myArr = [1, 2, 3, 4, 5];
+
+// console.log(typeof myArr);
+
+const myArr2 = new Array(7, 8, 3, 4, 5);
+
+// console.log(typeof myArr2);
+
+// const myArr3 = new Array(5);
+
+// console.log(myArr3[0]);
+
+// myArr.push(6);
+// myArr.pop();
+// myArr.unshift(0);
+// myArr.shift();
+// const newArr = myArr.join('*')
+// console.log(newArr);
+// console.log(typeof newArr);
+// console.log('A ', myArr);
+// const newArr = myArr.splice(0, 2);
+// console.log(newArr);
+// console.log('B ', myArr);
+// const newArr2 = myArr.splice(0, 2);
+// console.log(newArr2);
+// console.log('B ', myArr);
+
+// const newArr = myArr.concat(myArr2)
+
+// const newArr = [...myArr, ...myArr2]
+
+const newArr = [1, 2, 3, 4, [5, 6, 7], 8, [9, 10, [11, 12, 13]]];
+// const newArr2 = newArr.flat(Infinity);
+// console.log(newArr2);
+
+// console.log(Array.isArray('lkjh'));
+
+console.log(Array.of(2, 'e', true));
+
+console.log(Array.from('true'));
