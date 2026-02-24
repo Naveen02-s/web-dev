9bd38f5829e4576fa69d8050b0147b0ccd75c621
Naveen Sahu <145324584+Naveen02-s@users.noreply.github.com>
2026-02-24 08:38:00 +0000

    Objects completed

--- Javascript/010_objects/object.js
+++ Javascript/010_objects/object.js
@@ -1,11 +1,11 @@
-// const mySym = Symbol("key1")
+const mySym = Symbol("key1")
 
-// const obj = {
-//     userName: "kuch bhi",
-//     email: "kuchbhi@email.com",
-//     isLoggedIn: false,
-//     [mySym]: 123,
-// }
+const obj = {
+    userName: "kuch bhi",
+    email: "kuchbhi@email.com",
+    isLoggedIn: false,
+    [mySym]: 123,
+}
 
 // console.log(obj.email);
 
@@ -21,12 +21,20 @@
 // obj.email = 'kuchbhi@msoft.com'
 // console.log(obj);
 
-const obj1 = {1: 'a', 2: 'b'}
-const obj2 = {3: 'c', 4: 'd'}
+// const obj1 = {1: 'a', 2: 'b'}
+// const obj2 = {3: 'c', 4: 'd'}
 
 // const obj3 = {obj1, obj2}
 
 // const obj3 = Object.assign(obj1, obj2)
-const obj3 = Object.assign({}, obj1, obj2)
+// const obj3 = Object.assign({}, obj1, obj2)
 
-console.log(obj3);
+// const obj3 = Object.keys(obj1)
+
+// console.log(obj3);
+
+// const {userName} = obj
+// console.log(userName);
+
+const {userName: uName} = obj
+console.log(uName);
